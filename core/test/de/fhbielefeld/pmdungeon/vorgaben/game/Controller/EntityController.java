befdb4071274ffd22631a690961df228dc61a49f
ssteinmeyer <sebastian.steinmeyer@fh-bielefeld.de>
2021-06-08 23:27:25 +0200

    Setup junit an mockito in maven script

---
+++ core/test/de/fhbielefeld/pmdungeon/vorgaben/game/Controller/EntityController.java
@@ -0,0 +1,20 @@
+package de.fhbielefeld.pmdungeon.vorgaben.game.Controller;
+
+import org.junit.Test;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+public class EntityController {
+
+
+    @Test
+    public void setupTest() {
+        List mockedList = mock(List.class);
+        when(mockedList.get(0)).thenReturn("first");
+
+        assertEquals("first",mockedList.get(0));
+    }
+}
