ccd44133a1878efdf850b5683c2c8a7ce318ea05
CrappyAlgorithm <s.steinmeyer@unitybox.de>
2021-06-25 06:57:21 +0200

    Fixed error in test case.

--- core/test/de/fhbielefeld/pmdungeon/vorgaben/game/Controller/MainControllerTest.java
+++ core/test/de/fhbielefeld/pmdungeon/vorgaben/game/Controller/MainControllerTest.java
@@ -9,12 +9,8 @@ import de.fhbielefeld.pmdungeon.vorgaben.graphic.TextStage;
 import de.fhbielefeld.pmdungeon.vorgaben.tools.DungeonCamera;
 import org.junit.Test;
 import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-
-import java.util.List;
 
 import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 
@@ -30,12 +26,12 @@ public class MainControllerTest {
         HUD mHUD = mock(HUD.class);
         TextStage mTextStage = mock(TextStage.class);
 
-
         mMainController.levelController = mLevelController;
         mMainController.entityController = mEntityController;
         mMainController.camera = mDungeonCamera;
         mMainController.hud = mHUD;
         mMainController.textHUD = mTextStage;
+        mMainController.firstFrame = false;
         doCallRealMethod().when(mMainController).render(anyFloat());
 
         GL20 mGL = mock(GL20.class);
@@ -48,6 +44,7 @@ public class MainControllerTest {
             GameSetup.batch = mSpriteBatch;
             mMainController.render(1.0F);
         }
+
         verify(mGL).glClearColor(0, 0, 0, 1);
         verify(mGL).glClear(GL_COLOR_BUFFER_BIT);
         verify(mSpriteBatch).begin();
