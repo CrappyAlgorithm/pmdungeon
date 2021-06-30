bc2b0a19aaca2ad24367fea3c4af838faab6514e
Gysar <gysar.lp@gmail.com>
2021-06-30 09:29:21 +0200

    added 2 tests

--- core/test/de/fhbielefeld/pmdungeon/vorgaben/tools/DungeonCameraTest.java
+++ core/test/de/fhbielefeld/pmdungeon/vorgaben/tools/DungeonCameraTest.java
@@ -2,11 +2,14 @@ package de.fhbielefeld.pmdungeon.vorgaben.tools;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector3;
 import de.fhbielefeld.pmdungeon.vorgaben.interfaces.IDrawable;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyFloat;
+import static org.mockito.Mockito.*;
 
 
 public class DungeonCameraTest {
@@ -35,6 +38,38 @@ public class DungeonCameraTest {
         IDrawable follows=null;
         DungeonCamera camera = Mockito.spy(new DungeonCamera(null));
         assertEquals(camera.getFollowedObject(), null);
+    }
+
+    //35.3
+    @Test(expected = UnsatisfiedLinkError.class)
+    public void testUpdateFollowsNotNull() {
+        IDrawable follows=null;
+        DungeonCamera camera = Mockito.spy(new DungeonCamera(null));
+        camera.follow(new IDrawable() {
+            @Override
+            public Point getPosition() {
+                return new Point(1,1);
+            }
+
+            @Override
+            public Texture getTexture() {
+                return null;
+            }
+        });
+
+        camera.update();
+        verify(camera).position.set(anyFloat(),anyFloat(),0);
+    }
+
+    //35.4
+    @Test(expected = UnsatisfiedLinkError.class)
+    public void testUpdateFollowsNull() {
+        IDrawable follows=null;
+        DungeonCamera camera = new DungeonCamera(null);
+        camera.setFocusPoint(new Point(1,1));
+
+        camera.update();
+        verify(camera).position.set(1,1,0);
     }
 
 
